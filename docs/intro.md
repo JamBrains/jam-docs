bec0ef291407276cf486a8383a8ab0673be5012e
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-10-04 19:29:17 +0200

    fixes
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- docs/intro.md
+++ docs/intro.md
@@ -4,4 +4,6 @@ sidebar_position: 0
 slug: /
 ---
 
-Knowledge base of the JAM implementors teams.
+Collaborative docs from the JAM implementor teams alongside the [Testnet Repo](https://github.com/jam-duna/jamtestnet).
+
+The goal of this page is to provide information about all moving parts when building cross-team testnets.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/jambrains/jam-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jambrains/jam-docs/tree/main/',
           remarkPlugins: [math],
           rehypePlugins: [katex]
         },
