0399c00215fd37a0a25488306b904593e0069f8e
Oliver Tale-Yazdi <oliver@tasty.limo>
2024-12-01 14:40:58 +0100

    Fix links and add CI (#8)
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p node_modules
 wget https://raw.githubusercontent.com/zdave-parity/jam-np/refs/heads/main/simple.md -O node_modules/simple.md
 
-cp docs/advanced/networking/snp.md.tmpl docs/advanced/networking/snp.md
-cat node_modules/simple.md >> docs/advanced/networking/snp.md
+cp docs/advanced/simple-networking/spec.md.tmpl docs/advanced/simple-networking/spec.md
+cat node_modules/simple.md >> docs/advanced/simple-networking/spec.md
 
 yarn build
--- docs/advanced/jamnp-s/index.md
+++
@@ -1,6 +0,0 @@
----
-title: Simple QUIC Networking
-sidebar_position: 1
----
-
-The JAM networking spec defines a the [Simple Networking Protocol](./snp.md) for testing. This version of the protocol will most likely not be formalized in the Graypaper. The Graypaper will only define the full version of the networking protocol once it is finalized.
