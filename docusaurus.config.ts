b2fb0650c578523e1bde2281a296800a6cb01163
Oliver Tale-Yazdi <oliver@tasty.limo>
2025-03-03 21:01:55 +0100

    Update deps
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -65,7 +65,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/jam-chain-docs-banner.png',
     navbar: {
       title: 'JAM Docs',
