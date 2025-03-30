efd60f778ad7ac0cbcbd76519216d3ccf049b539
Oliver Tale-Yazdi <oliver@tasty.limo>
2025-03-30 13:34:47 +0200

    Fix links
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ mkdir -p node_modules
 wget https://raw.githubusercontent.com/zdave-parity/jam-np/refs/heads/main/simple.md -O node_modules/simple.md
 wget https://hackmd.io/@polkadot/jip1/download -O node_modules/host-call-log.md
 
-cp docs/advanced/simple-networking/spec.md.tmpl docs/advanced/simple-networking/spec.md
-cat node_modules/simple.md >> docs/advanced/simple-networking/spec.md
-perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/advanced/simple-networking/spec.md
+cp docs/knowledge/advanced/simple-networking/spec.md.tmpl docs/knowledge/advanced/simple-networking/spec.md
+cat node_modules/simple.md >> docs/knowledge/advanced/simple-networking/spec.md
+perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/knowledge/advanced/simple-networking/spec.md
 
-cp docs/testing/polka-vm/host-call-log.md.tmpl docs/testing/polka-vm/host-call-log.md
-cat node_modules/host-call-log.md >> docs/testing/polka-vm/host-call-log.md
-perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/testing/polka-vm/host-call-log.md
+cp docs/knowledge/testing/polka-vm/host-call-log.md.tmpl docs/knowledge/testing/polka-vm/host-call-log.md
+cat node_modules/host-call-log.md >> docs/knowledge/testing/polka-vm/host-call-log.md
+perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/knowledge/testing/polka-vm/host-call-log.md
 
 npm run convert-yaml docs/dao/index.md
 
