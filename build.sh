c28b00eb475494d764385fe3bcd98b7cbe5f4424
Oliver Tale-Yazdi <oliver@tasty.limo>
2025-03-30 13:24:36 +0200

    Fix build date in templates
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- build.sh
+++ build.sh
@@ -8,9 +8,11 @@ wget https://hackmd.io/@polkadot/jip1/download -O node_modules/host-call-log.md
 
 cp docs/advanced/simple-networking/spec.md.tmpl docs/advanced/simple-networking/spec.md
 cat node_modules/simple.md >> docs/advanced/simple-networking/spec.md
+perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/advanced/simple-networking/spec.md
 
 cp docs/testing/polka-vm/host-call-log.md.tmpl docs/testing/polka-vm/host-call-log.md
 cat node_modules/host-call-log.md >> docs/testing/polka-vm/host-call-log.md
+perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/testing/polka-vm/host-call-log.md
 
 npm run convert-yaml docs/dao/index.md
 
