f459a41ef728a208e6b3d34e6d4790770c761a86
Oliver Tale-Yazdi <oliver@tasty.limo>
2025-09-19 22:33:51 +0300

    fix
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ wget https://raw.githubusercontent.com/zdave-parity/jam-np/refs/heads/main/simpl
 wget https://hackmd.io/@polkadot/jip1/download -O node_modules/host-call-log.md
 wget https://hackmd.io/@polkadot/jip2/download -O node_modules/jip2-gav.md
 
-cp docs/knowledge/advanced/simple-networking/spec.md.tmpl docs/knowledge/advanced/simple-networking/spec.md
-cat node_modules/simple.md >> docs/knowledge/advanced/simple-networking/spec.md
-perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/knowledge/advanced/simple-networking/spec.md
+cp docs/knowledge/simple-networking/spec.md.tmpl docs/knowledge/simple-networking/spec.md
+cat node_modules/simple.md >> docs/knowledge/simple-networking/spec.md
+perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/knowledge/simple-networking/spec.md
 
 cp docs/knowledge/testing/pvm/host-call-log.md.tmpl docs/knowledge/testing/pvm/host-call-log.md
 cat node_modules/host-call-log.md >> docs/knowledge/testing/pvm/host-call-log.md
 perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/knowledge/testing/pvm/host-call-log.md
 
-cp docs/knowledge/advanced/rpc/jip2-gav.md.tmpl docs/knowledge/advanced/rpc/jip2-gav.md
-cat node_modules/jip2-gav.md >> docs/knowledge/advanced/rpc/jip2-gav.md
-perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/knowledge/advanced/rpc/jip2-gav.md
+cp docs/knowledge/rpc/jip2-gav.md.tmpl docs/knowledge/rpc/jip2-gav.md
+cat node_modules/jip2-gav.md >> docs/knowledge/rpc/jip2-gav.md
+perl -pi -e "s/BUILD_DATE/$(date +%Y-%m-%d)/" docs/knowledge/rpc/jip2-gav.md
 
 npm run convert-yaml docs/dao/index.md
 
